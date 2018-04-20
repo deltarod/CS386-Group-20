964c7b1f14e76300e122eb1c946f4d02376a1b3d
Tristan <tristanmillerschool@gmail.com>
2018-04-19 17:08:16 -0700

    Added Unit/Integration tests

--- Code/homepage/tests.py
+++ Code/homepage/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from .models import School, Profile
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class TestWebsite(TestCase):
+
+    #Unit test to create a user
+    def createUser(self):
+        print("Creating a user object")
+        return User.objects.create(username="TestUsername")
+
+    #Unit test to create a school
+    def createSchool(self):
+        print("Creating a school object")
+        return School.objects.create(schoolName="TestSchool", location="A Place")
+
+
+    # Integration test to test that the algorithm for joining schools works
+    def testSchoolJoin(self):
+        profileInstance = self.createUser()
+
+        schoolinstance = self.createSchool()
+
+        print("Unit test for school join running")
+
+        profileInstance.college = schoolinstance
+
+        self.assertIs( profileInstance.college, schoolinstance)
+
+
